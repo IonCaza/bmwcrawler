4b195c388c31ae6ce5dbda644c40a5f8c9302bdc
Ion Caza <contact@johncaza.com>
2020-05-02 01:27:57 -0400

    Adds all the stuff

--- crawlAllVehiclesToMongo.js
+++ crawlAllVehiclesToMongo.js
@@ -5,37 +5,55 @@ radius = 3500
 zip = 48331
 yearStart = 2019
 yearEnd = 2019
-minPrice = 75000
-showOnlyDemo = 0
+minPrice = 98000
+maxPrice = 120000
 
+// Export this stuff
+exportFromVehicle = 
+    ['name', 
+    'vin', 
+    'totalMsrp', 
+    'dealer.businessName', 
+    'dealer.phone', 
+    'dealer.url']
+    
 const url = 'https://www.bmwusa.com/api/inventory/filter/search/'
 vehicles = ''
 
 axios.post(url + zip + '?page=1&resultsPerPage=500&sortBy=PRICE&sortOrder=ASC', 
 {
     //selectedSeries: ["M"],
-   // selectedModels: ["M5"],
+    //selectedModels: ["M5"],
     selectedMinPrice: minPrice,
+    selectedMinPrice: maxPrice,
     selectedLocatorRange: radius,
-    minModelYear: yearStart,
-    maxModelYear: yearEnd,
     selectedMinModelYear: yearStart,
     selectedMaxModelYear: yearEnd,
     selectedModelYears: [yearStart]
 })
 .then((response) => {
     vehicles = response.data;
-    democount = 0;
-    console.log('Total number of vehicles: ' + vehicles.totalVehicleCount);
+    pipedHeaders = ''
+    exportFromVehicle.forEach(element => pipedHeaders += element + '|')
+    console.log(pipedHeaders)
     for (let step = 0; step < vehicles.totalVehicleCount; step++) {
-        if (vehicles.vehicles[step].demonstrator == true) {
-            democount++;
-            console.log(vehicles.vehicles[step].name + "|" + vehicles.vehicles[step].vin + "|" + vehicles.vehicles[step].totalMsrp);
-        }else if (showOnlyDemo != 1) {
-        console.log(vehicles.vehicles[step].name + "|" + vehicles.vehicles[step].vin + "|" + vehicles.vehicles[step].totalMsrp);
+        x = vehicles.vehicles[step];
+        u = exportFromVehicle.map(exportFunk)
+
+        function exportFunk(value, index, array) {        
+            return pathIndex(x, value)
         }
+        function multiIndex(obj,is) {
+            return is.length ? multiIndex(obj[is[0]],is.slice(1)) : obj
+        }
+        function pathIndex(obj,is) {
+            return multiIndex(obj,is.split('.'))
+        }
+        pipedResults = ''
+        u.forEach(element => pipedResults += element + '|')
+        console.log(pipedResults)
     }
-    console.log('Number of demos: ' + democount);
+    console.log('Total number of vehicles|' + vehicles.totalVehicleCount);
 
 }, (error) => {
     console.log(error);
