4549cc2b6fbf9b44dd9654a57bc8f99cabc8db82
Ion Caza <contact@johncaza.com>
2020-05-01 23:21:24 -0400

    New pipes

--- crawlAllVehiclesToMongo.js
+++ crawlAllVehiclesToMongo.js
@@ -1,20 +1,41 @@
 const axios = require('axios');
 
+// Search For
+radius = 3500
+zip = 48331
+yearStart = 2019
+yearEnd = 2019
+minPrice = 75000
+showOnlyDemo = 0
+
 const url = 'https://www.bmwusa.com/api/inventory/filter/search/'
 vehicles = ''
 
-axios.post(url + '48331?page=1&resultsPerPage=24&sortBy=PRICE&sortOrder=ASC', 
+axios.post(url + zip + '?page=1&resultsPerPage=500&sortBy=PRICE&sortOrder=ASC', 
 {
-    selectedSeries: ["M"],
-    selectedModels: ["M5"],
-    selectedLocatorRange: 150
+    //selectedSeries: ["M"],
+   // selectedModels: ["M5"],
+    selectedMinPrice: minPrice,
+    selectedLocatorRange: radius,
+    minModelYear: yearStart,
+    maxModelYear: yearEnd,
+    selectedMinModelYear: yearStart,
+    selectedMaxModelYear: yearEnd,
+    selectedModelYears: [yearStart]
 })
 .then((response) => {
-    vehicles = response.data.vehicles;
-    // iterate through vehicles and throw it in mongo
-    
-    console.log(vehicles[0].dealer);
-
+    vehicles = response.data;
+    democount = 0;
+    console.log('Total number of vehicles: ' + vehicles.totalVehicleCount);
+    for (let step = 0; step < vehicles.totalVehicleCount; step++) {
+        if (vehicles.vehicles[step].demonstrator == true) {
+            democount++;
+            console.log(vehicles.vehicles[step].name + "|" + vehicles.vehicles[step].vin + "|" + vehicles.vehicles[step].totalMsrp);
+        }else if (showOnlyDemo != 1) {
+        console.log(vehicles.vehicles[step].name + "|" + vehicles.vehicles[step].vin + "|" + vehicles.vehicles[step].totalMsrp);
+        }
+    }
+    console.log('Number of demos: ' + democount);
 
 }, (error) => {
     console.log(error);
--- crawlEachVehicleToMongo.js
+++ crawlEachVehicleToMongo.js
@@ -3,13 +3,13 @@ const axios = require('axios');
 const url = 'https://www.bmwusa.com/api/inventory/vehicle/'
 vehicle = ''
 
-axios.get(url + 'WBSJF0C07LB449045', 
+axios.get(url + 'WBSJF0C58KB447654', 
 {
 })
 .then((response) => {
     vehicle = response.data;
     
-    console.log(vehicle);
+    console.log(vehicle.options);
 
 
 }, (error) => {
